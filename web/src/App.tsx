bb3aa77a7e760c89428f97101821e95239cb333b
GersonST <gersonsevero90@gmail.com>
2021-07-23 19:58:15 -0300

    Landing Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import "./styles/pages/landing.css";
+import "./styles/global.css";
+import { FiArrowRight } from "react-icons/fi";
+
+import logoImg from "./assets/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Happy</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Porto</strong>
+          <span>Portugal</span>
+        </div>
+
+        <a href="/" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
